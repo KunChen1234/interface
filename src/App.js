7563402d73a2ea5aa7f87f5716a246922421d07d
ck <ckjohn0107@gmail.com>
2022-08-29 17:22:08 +1000

    second

--- src/App.js
+++ src/App.js
@@ -2,23 +2,24 @@ import './App.css';
 
 function App() {
   return (
-    <div>
+    <div class="grid grid-flow-4 h-screen">
       <div class="bg-black grid grid-cols-3 gap-2">
         <div class="text-white text-sm">aa</div>
         <div class="text-center ">
-          <p class="text-white text-lg">ck</p>
+          <p class="text-white text-sm">ck</p>
         </div>
         <div class="grid grid-flow-3 text-white text-sm text-right">
-          <div class="row-span-2 text-7xl">bb</div>
+          <div class="row-span-2 text-sm">bb</div>
           <div class="mb-0">dd</div>
         </div>
       </div>
-      <div>
-        <div class="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4">
+      <div class="row-span-2 items-center">
+        <div class="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg  items-center space-x-4">
           <p class="text-xl font-medium text-primary">ChitChat</p>
           <p class="text-secondary">You have a new message!</p>
         </div>
       </div>
+      <div class="bg-black">bottom</div>
     </div>
   );
 }
